22cef4180cdbb053a0dd1c5debe8af9cf74ebe9d
Holger Wolff <holger.wolff@monash.edu>
2014-08-25 18:24:01 +1000

    created a python script to calculate day difference

---
+++ calc_difference.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from datetime import datetime
+from optparse import OptionParser
+
+def calc_difference(old, new):
+  """ (date, date) -> int
+  returns the number of days between old and new
+
+  >>> old = datetime.strptime('2000-01-01', '%Y-%m-%d')
+  >>> new = datetime.strptime('2000-01-02', '%Y-%m-%d')
+  >>> calc_difference(old, new)
+  1
+  >>> new = datetime.strptime('2000-02-01', '%Y-%m-%d')
+  >>> calc_difference(old, new)
+  31
+  """
+  return (new-old).days
+
+def main():
+  parser=OptionParser()
+  parser.add_option('-o', '--original', help='Original date')
+  parser.add_option('-n', '--new', help='New date')
+  (opts, args) = parser.parse_args()
+
+  old_time = datetime.strptime(opts.original, '%Y-%m-%d %H:%M')
+  new_time = datetime.strptime(opts.new, '%Y-%m-%d %H:%M')
+  print calc_difference(old_time, new_time)
+
+if __name__ == '__main__':
+  #import doctest
+  #doctest.testmod()
+  main()
+
